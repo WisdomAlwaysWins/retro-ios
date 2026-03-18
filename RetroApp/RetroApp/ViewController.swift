c613a79c6a9803efd898615c40067c153d75f4a1
J <knowisdom314@gmail.com>
2026-03-19 04:38:59 +0900

    🍀 Chore: SwiftLint, SwiftFormat 적용

--- RetroApp/RetroApp/ViewController.swift
+++ RetroApp/RetroApp/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  RetroApp
-//
-//  Created by J on 3/19/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
