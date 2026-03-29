caa4bf70bbff9c59b0b037a57e2fe338ee070341
J <knowisdom314@gmail.com>
2026-03-30 03:18:43 +0900

    ✨ Feat: Controller 연결

--- RetroApp/RetroApp/Application/SceneDelegate.swift
+++ RetroApp/RetroApp/Application/SceneDelegate.swift
@@ -3,21 +3,19 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene
-        // `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see
-        // `application:configurationForConnectingSceneSession` instead).
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIViewController()
-        window?.makeKeyAndVisible()
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        let navigationController = UINavigationController()
+        let diContainer = DIContainer()
+        let coordinator = AppCoordinator(
+            navigationController: navigationController,
+            diContainer: diContainer,
+            window: window
+        )
+        coordinator.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
