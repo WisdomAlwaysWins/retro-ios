2be34843fdda416748837c83e392a4697cc4d156
J <knowisdom314@gmail.com>
2026-03-30 03:17:35 +0900

    ✨ Feat: Coordinator 구현

---
+++ RetroApp/RetroApp/Presentation/Common/Coordinator/AppCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  AppCoordinator.swift
+//  RetroApp
+//
+//  Created by J on 3/30/26.
+//
+
+import Foundation
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    private let diContainer: DIContainer
+    private let window: UIWindow
+
+    init(navigationController: UINavigationController, diContainer: DIContainer, window: UIWindow) {
+        self.navigationController = navigationController
+        self.diContainer = diContainer
+        self.window = window
+    }
+
+    func start() {
+        let viewModel = RetrospectListViewModel(fetchRetrospectListUseCase: diContainer.fetchRetrospectListUseCase)
+
+        let viewController = RetrospectListViewController(viewModel: viewModel)
+        navigationController.viewControllers = [viewController]
+
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
